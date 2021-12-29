d807118485c4cab32c4af10cfbb11da3e319079d
Alex <c23026730@gmail.com>
2021-12-29 11:39:03 +0800

    依需求朴整表格與呈現資料

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,8 +1,39 @@
 <template>
 	<v-container>
-		<v-row class="text-center">
-			<v-data-table :items="countryList.datas" :headers="countryList.headers"></v-data-table>
-		</v-row>
+		<v-card>
+			<v-card-title>
+				國名查詢
+				<!-- <v-spacer></v-spacer> -->
+				<v-text-field
+					v-model="countryList.search"
+					append-icon="mdi-magnify"
+					label="Search"
+					single-line
+					hide-details
+					class="pt-0 pb-4 pl-5"
+				></v-text-field>
+			</v-card-title>
+			<v-data-table 
+				fixed-header height="1200px" 
+				:items="countryList.datas" 
+				:headers="countryList.headers" 
+				:search="countryList.search"
+				items-per-page="25"
+				:sort-by="['name.official']"
+				:sort-desc="[true]"
+			>
+				<template v-slot:item.flags.png="{ item }">
+					<img :src="item.flags.png" :alt="item.name.official" class="flag">
+				</template>
+				<template v-slot:item.nativeName="{ item }">
+					<span v-for="(n, key) in item.nativeName" :key="key">{{ n }}</span>
+				</template>
+				<template v-slot:item.idd="{ item }">
+					<span class="mr-1">{{item.idd.root}}</span>
+					<span v-for="(n, key) in item.idd.suffixes" :key="key">{{ n }}</span>
+				</template>
+			</v-data-table>
+		</v-card>
 	</v-container>
 </template>
 
--- src/store/CountryList/modules/index.js
+++ src/store/CountryList/modules/index.js
@@ -8,100 +8,15 @@ const state = {
         countryList: {
             search: '',
             headers: [
-                {
-                    text: 'Dessert (100g serving)',
-                    align: 'start',
-                    filterable: false,
-                    value: 'name',
-                },
-                { text: 'Calories', value: 'calories' },
-                { text: 'Fat (g)', value: 'fat' },
-                { text: 'Carbs (g)', value: 'carbs' },
-                { text: 'Protein (g)', value: 'protein' },
-                { text: 'Iron (%)', value: 'iron' },
+                { text: '國家名稱',value: 'name.official' },
+                { text: '國旗', value: 'flags.png' },
+                { text: 'cca2', value: 'cca2' },
+                { text: 'cca3', value: 'cca3' },
+                { text: '母語名稱', value: 'nativeName' },
+                { text: '替代國家名稱', value: 'altSpellings' },
+                { text: '國際電話區號', value: 'idd' },
             ],
-            datas: [
-            {
-                name: 'Frozen Yogurt',
-                calories: 159,
-                fat: 6.0,
-                carbs: 24,
-                protein: 4.0,
-                iron: '1%',
-            },
-            {
-                name: 'Ice cream sandwich',
-                calories: 237,
-                fat: 9.0,
-                carbs: 37,
-                protein: 4.3,
-                iron: '1%',
-            },
-            {
-                name: 'Eclair',
-                calories: 262,
-                fat: 16.0,
-                carbs: 23,
-                protein: 6.0,
-                iron: '7%',
-            },
-            {
-                name: 'Cupcake',
-                calories: 305,
-                fat: 3.7,
-                carbs: 67,
-                protein: 4.3,
-                iron: '8%',
-            },
-            {
-                name: 'Gingerbread',
-                calories: 356,
-                fat: 16.0,
-                carbs: 49,
-                protein: 3.9,
-                iron: '16%',
-            },
-            {
-                name: 'Jelly bean',
-                calories: 375,
-                fat: 0.0,
-                carbs: 94,
-                protein: 0.0,
-                iron: '0%',
-            },
-            {
-                name: 'Lollipop',
-                calories: 392,
-                fat: 0.2,
-                carbs: 98,
-                protein: 0,
-                iron: '2%',
-            },
-            {
-                name: 'Honeycomb',
-                calories: 408,
-                fat: 3.2,
-                carbs: 87,
-                protein: 6.5,
-                iron: '45%',
-            },
-            {
-                name: 'Donut',
-                calories: 452,
-                fat: 25.0,
-                carbs: 51,
-                protein: 4.9,
-                iron: '22%',
-            },
-            {
-                name: 'KitKat',
-                calories: 518,
-                fat: 26.0,
-                carbs: 65,
-                protein: 7,
-                iron: '6%',
-            },
-            ],
+            datas: [],
         }
     }
 }
@@ -118,14 +33,24 @@ const actions = {
     getCountryListData: async (context) => {
         axios(context.state.apiList.all).then(response => {
             console.log(response)
-            // if(response.data.status != true){
-            //     throw response.data 
-            // }
-            // if(response.data.result[0].member_character.length > 0){
-            //     response.data.result[0].member_character.forEach((el) => {
-            //         context.state.selectList.ownedCharacterList.push(el.character_no)
-            //     })
-            // }
+            if(response.data.length < 0 ){
+                throw response.data 
+            }
+            context.state.dataList.countryList.datas = response.data
+            context.state.dataList.countryList.datas.sort((a, b) => {
+                return a.Brand > b.Brand ? 1 : -1;
+            });
+            context.state.dataList.countryList.datas.forEach((el) => {
+                el.nativeName = []
+                if(el.name.nativeName != null && el.name.nativeName != undefined){
+                    Object.keys(el.name.nativeName).forEach((k) => {
+                        el.nativeName.push(el.name.nativeName[k].official)
+                    })
+                }
+                // let k = el.cioc.toLowerCase()
+                // console.log(k)
+                // console.log(el.name.nativeName[k].official)
+            })
             // if(response.data.result[0].member_shopspace.length > 0){
             //     response.data.result[0].member_shopspace.forEach((el) => {
             //         context.state.selectList.ownedShopspaceList.push(el)
